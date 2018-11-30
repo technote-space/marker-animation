ee10f8ef4a22aa296c3497a393cf32c2a0ee1601
technote-space <technote.space@gmail.com>
2018-11-30 18:49:42 +0900

    v1.0.1

--- marker-animation.php
+++ marker-animation.php
@@ -4,7 +4,7 @@ Plugin Name: Marker Animation
 Plugin URI:
 Description: This plugin will add "Marker animation" function
 Author: technote
-Version: 1.0.0
+Version: 1.0.1
 Author URI: https://technote.space
 Text Domain: marker-animation
 Domain Path: /languages/
