4c35528931a10f2b02315c338588694359a53617
technote-space <technote.space@gmail.com>
2018-12-09 06:27:06 +0900

    v1.2.0

--- configs/filter.php
+++ configs/filter.php
@@ -1,8 +1,9 @@
 <?php
 /**
- * @version 1.1.9
+ * @version 1.2.0
  * @author technote-space
  * @since 1.0.0
+ * @since 1.2.0
  * @copyright technote All Rights Reserved
  * @license http://www.opensource.org/licenses/gpl-2.0.php GNU General Public License, version 2
  * @link https://technote.space/
