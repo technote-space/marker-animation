56307a7a932ae59878935aa3515897a434cd922b
technote <technote.space@gmail.com>
2019-03-04 17:48:09 +0900

    v1.6.10

--- src/classes/models/editor.php
+++ src/classes/models/editor.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @version 1.6.4
+ * @version 1.6.10
  * @author Technote
  * @since 1.0.0
  * @since 1.2.0
@@ -13,6 +13,7 @@
  * @since 1.5.0 Changed: ライブラリの変更 (#37)
  * @since 1.6.0 Changed: Gutenbergへの対応 (#3)
  * @since 1.6.4 Changed: 有効でない場合にエディタにボタンを追加しない (#61)
+ * @since 1.6.10 #93
  * @copyright Technote All Rights Reserved
  * @license http://www.opensource.org/licenses/gpl-2.0.php GNU General Public License, version 2
  * @link https://technote.space/
