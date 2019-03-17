8a02f4f4e02fc8ecf34deeb2f7f985b4ee2db281
technote <technote.space@gmail.com>
2019-03-18 02:34:47 +0900

    #102

---
+++ configs/preset.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * @version 1.7.1
+ * @author Technote
+ * @since 1.7.1
+ * @copyright Technote All Rights Reserved
+ * @license http://www.opensource.org/licenses/gpl-2.0.php GNU General Public License, version 2
+ * @link https://technote.space/
+ */
+
+if ( ! defined( 'MARKER_ANIMATION' ) ) {
+	exit;
+}
+
+return [
+
+	[
+		'name'     => 'Red',
+		'color'    => '#f69',
+		'priority' => 50,
+	],
+
+	[
+		'name'     => 'Green',
+		'color'    => '#6f6',
+		'priority' => 50,
+	],
+
+	[
+		'name'     => 'Blue',
+		'color'    => '#6cf',
+		'priority' => 50,
+	],
+
+];
