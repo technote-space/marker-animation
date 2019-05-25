1a116091d3f66d5b7460552b56fabf38750e39ce
Technote <technote.space@gmail.com>
2019-05-26 02:51:01 +0900

    #131

--- src/views/admin/style/custom_post/setting.php
+++ src/views/admin/style/custom_post/setting.php
@@ -8,21 +8,18 @@
  * @link https://technote.space/
  */
 
+use WP_Framework_Presenter\Interfaces\Presenter;
+
 if ( ! defined( 'MARKER_ANIMATION' ) ) {
 	return;
 }
+/** @var Presenter $instance */
+/** @var array $args */
+$instance->add_style_view( 'admin/style/dashboard', $args );
 ?>
 <style>
     .marker-setting-preview {
         font-size: 1.5em;
         line-height: 2em;
-    }
-
-    select[data-readonly="1"] {
-        opacity: .5;
-    }
-
-    select[data-readonly="1"] option {
-        display: none;
     }
 </style>
