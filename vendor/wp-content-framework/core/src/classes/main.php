336ebe1db702fbc96124235165f78b14dfc2d8bf
Technote <technote.space@gmail.com>
2019-06-02 04:40:44 +0900

    wp-content-framework/common#107 wp-content-framework/update#22

--- vendor/wp-content-framework/core/src/classes/main.php
+++ vendor/wp-content-framework/core/src/classes/main.php
@@ -2,7 +2,7 @@
 /**
  * WP_Framework_Core Classes Main
  *
- * @version 0.0.54
+ * @version 0.0.56
  * @author Technote
  * @copyright Technote All Rights Reserved
  * @license http://www.opensource.org/licenses/gpl-2.0.php GNU General Public License, version 2
@@ -322,6 +322,9 @@ class Main {
 		$this->_initialized = true;
 
 		$this->filter->do_action( 'app_initialize', $this );
+		if ( $this->app->is_uninstall() ) {
+			$this->system;
+		}
 	}
 
 	/**
@@ -350,16 +353,16 @@ class Main {
 	}
 
 	/**
-	 * @deprecated
 	 * @return string
+	 * @deprecated
 	 */
 	public function get_plugin_uri() {
 		return $this->app->get_plugin_data( $this->app->is_theme ? 'ThemeURI' : 'PluginURI' );
 	}
 
 	/**
-	 * @deprecated
 	 * @return array
+	 * @deprecated
 	 */
 	public function get_package_versions() {
 		return $this->app->array->combine( array_map( function ( $package ) {
