df5c11d2b061a3cea000edfba01c75d5a8c5e670
Technote <technote.space@gmail.com>
2020-04-04 04:10:35 +0900

    chore: update wp version

--- marker-animation.php
+++ marker-animation.php
@@ -4,7 +4,7 @@
  * Plugin URI: https://github.com/technote-space/marker-animation
  * Description: This plugin will add "Marker animation" function
  * Author: Technote
- * Version: 3.0.0
+ * Version: 3.0.1
  * Author URI: https://technote.space
  * Text Domain: marker-animation
  * Domain Path: /languages/
