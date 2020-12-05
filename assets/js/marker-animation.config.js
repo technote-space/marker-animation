6a9cac9903b622dce6ef541c7b5b95693003a419
Technote <technote.space@gmail.com>
2020-12-05 13:04:07 +0900

    chore: tweaks

--- assets/js/marker-animation.config.js
+++ assets/js/marker-animation.config.js
@@ -1,6 +1,5 @@
 const SpeedMeasurePlugin            = require('speed-measure-webpack-plugin');
 const DuplicatePackageCheckerPlugin = require('duplicate-package-checker-webpack-plugin');
-const HardSource                    = require('hard-source-webpack-plugin');
 const smp                           = new SpeedMeasurePlugin();
 const webpack                       = require('webpack');
 const pkg                           = require('./package');
@@ -20,7 +19,7 @@ const webpackConfig = {
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        loader: 'babel-loader?cacheDirectory',
+        loader: 'babel-loader',
       },
     ],
   },
@@ -30,7 +29,6 @@ const webpackConfig = {
   plugins: [
     new webpack.BannerPlugin(banner),
     new DuplicatePackageCheckerPlugin(),
-    new HardSource(),
   ],
 };
 
